071304204f62f7e04b69d931925efb89ede5328b
hkladoi <anhtrinhtran3@gmail.com>
2023-03-15 11:14:40 +0700

    a

--- Mixi/Controllers/HomeController.cs
+++ Mixi/Controllers/HomeController.cs
@@ -22,6 +22,55 @@ namespace Mixi.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            Product product = new Product() { ProductID = Guid.NewGuid(), Name = "hkladoi", ProductCode = "HK1", Price = 1000, SalePrice = 100, AvailableQuantity = 1, Supplier = "HN", Description = "co san", Status = 0 };
+            return View(product);
+        }
+        public IActionResult ShowlistProduct()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() {
+                    ProductID = Guid.NewGuid(),
+                    Name = "hkladoi",
+                    ProductCode = "HK1",
+                    Price = 1000,
+                    SalePrice = 100,
+                    AvailableQuantity = 1,
+                    Supplier = "HN",
+                    Description = "co san",
+                    Status = 0
+                },
+                new Product() {
+                    ProductID = Guid.NewGuid(),
+                    Name = "hkladoi",
+                    ProductCode = "HK1",
+                    Price = 1000,
+                    SalePrice = 100,
+                    AvailableQuantity = 1,
+                    Supplier = "HN",
+                    Description = "co san",
+                    Status = 0
+                },
+            };
+            return View(products);
+        }
+
+        public IActionResult Details(Guid id)
+        {
+            MixiDbContext dbContext = new MixiDbContext();
+            var product = dbContext.Products.Find(id);
+            return View(product);
+        }
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Test");//thực hiện điều hướng đến 1 action nào đó
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
